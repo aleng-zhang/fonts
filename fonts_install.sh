aac1e32030879abbaee8d9a87814b2ec0002a996
aleng_zhang <951532104@qq.com>
2017-07-08 20:06:23 +0000

    Upload

---
+++ fonts_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo cp -r ~/aleng_fonts /usr/share/fonts/aleng_fonts
+sudo chmod 755 /usr/share/fonts/aleng_fonts
+sudo chmod 644 /usr/share/fonts/aleng_fonts/*
+sudo mkfontdir&&sudo mkfontscale&&sudo fc-cache -fv
