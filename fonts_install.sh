aaa70cf596a68da57f42be7c01aae491f561857c
aleng_zhang <aleng_zhang@outlook.com>
2018-02-25 12:18:28 +0800

    update

--- fonts_install.sh
+++ fonts_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp -r ./aleng_fonts /usr/share/fonts/aleng_fonts
-sudo chmod 755 /usr/share/fonts/aleng_fonts
-sudo chmod 644 /usr/share/fonts/aleng_fonts/*
+sudo cp -r ./add_fonts /usr/share/fonts/add_fonts
+sudo chmod 755 /usr/share/fonts/add_fonts
+sudo chmod 644 /usr/share/fonts/add_fonts/*
 sudo mkfontdir&&sudo mkfontscale&&sudo fc-cache -fv
